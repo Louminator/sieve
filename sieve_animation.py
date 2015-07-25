fd06c5d0fe95115a2cba38d44925cc1117573275
Louminator <rossi@math.udel.edu>
2015-07-25 16:34:14 -0400

    Broke out animation code into a class extending a simple experiment.

---
+++ sieve_animation.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on July 2015
+
+@author: rossi
+
+A sieve simulation program.
+"""
+
+from scipy import *
+from numpy.random import uniform,permutation
+
+import matplotlib.backends.backend_agg as agg
+import pygame
+import matplotlib.pyplot as plt
+
+#from sieve_experiment import sieve_experiment
+
+from sieve_experiment import *
+
+class sieve_animate(sieve_experiment):
+    def __init__(self,m=110,n=10,iterations=300,size = (600,500)):
+        
+        sieve_experiment.__init__(self,m,n,iterations)
+        
+        self.size = size
+
+        pygame.init()
+        self.disp = pygame.display.set_mode(self.size)
+        self.disp.fill((0,0,0))
+        
+#        self.pos,self.arrangement = self.DrawSieve()
+        self.DrawSieve()
+        
+    def play(self):
+        for its in range(1,self.iterations):
+            fig,raw_data,canvas = self.CreatePlot(its)
+            
+            if (its == 1):
+                self.size2 = canvas.get_width_height()
+                
+            surf = pygame.image.fromstring(raw_data, self.size2, "ARGB")
+            if (self.smallPores[its-1] == self.smallPores[its]):
+                # Fall into the next large pore.
+                q = int(uniform(0,self.m+self.n))
+                while (self.arrangement[q] >= self.n):
+                    q = int(uniform(0,self.m+self.n))
+                self.ParticleFall(q,surf)
+            else:
+                # Fall into the next small pore.
+                idx = argwhere(self.arrangement == self.n+self.smallPores[its])
+                idx = idx[0][0]
+                self.ParticleStuck(idx,surf)
+            plt.close(fig)
+        
+    def DrawSieve(self):
+        self.pos = map(int,self.size[0]/(m+n+1.)*r_[0:self.m+self.n]+2.5)
+        self.arrangement = permutation(self.n+self.m)
+        
+        pygame.draw.rect(self.disp,(0,255,0),(0,self.size[1]-20,self.size[0],20))
+        for k in arange(len(self.pos)):
+            if (self.arrangement[k] >= self.n):
+                pygame.draw.rect(self.disp,(0,0,0),(self.pos[k],self.size[1]-20,1,20))
+            else:
+                pygame.draw.rect(self.disp,(0,0,0),(self.pos[k]-1,self.size[1]-20,3,20))
+        pygame.display.flip()
+#        return(pos,arrangement)
+
+    def CreatePlot(self,ind):
+        fig = plt.figure(figsize=[self.size[0]/100, (self.size[1]-40)/100], dpi=100)
+        ax = fig.gca()
+        ax.plot(self.s,self.p,'c')
+        ax.plot(self.sims[:ind],self.simp[:ind],'xb')
+        plt.xlabel('time (t)',fontsize=12)
+        plt.title('Probability',fontsize=12)
+        ax.legend(('Theory','Experiment'),numpoints=1)
+        canvas = agg.FigureCanvasAgg(fig)
+        canvas.draw()
+        renderer = canvas.get_renderer()
+        raw_data = renderer.tostring_argb()
+        return(fig,raw_data,canvas)
+    
+    def ParticleFall(self,ind,surf):
+        for j in range(0,self.size[1],2):
+            self.disp.blit(surf, ((self.size[0]-self.size2[0])/2,0))
+            x = int((self.pos[ind]+self.size[0]/2.)/2. + (self.size[0]/2.-self.pos[ind])*(cos(pi*j/self.size[1]))/2.)
+            pygame.draw.rect(self.disp,(200,0,200),(x,j,3,3))
+            pygame.display.flip()
+            pygame.draw.rect(self.disp,(0,0,0),(x,j,3,3))
+            
+    def ParticleStuck(self,ind,surf):
+        for j in range(0,self.size[1]-21,2):
+            self.disp.blit(surf, ((self.size[0]-self.size2[0])/2,0))
+            x = int((self.pos[ind]+self.size[0]/2.)/2. + (self.size[0]/2.-self.pos[ind])*(cos(pi*j/(self.size[1]-21)))/2.)
+            pygame.draw.rect(self.disp,(200,0,200),(x,j,3,3))
+            pygame.display.flip()
+            pygame.draw.rect(self.disp,(0,0,0),(x,j,3,3))
+        pygame.draw.rect(self.disp,(255,0,0),(self.pos[ind],self.size[1]-23,2,2))
+        pygame.display.flip()
+        
+experiment = sieve_animate()
+experiment.play()
--- sieve_experiment.py
+++ sieve_experiment.py
@@ -9,16 +9,13 @@ A sieve simulation program.
 
 from scipy import *
 from scipy.special import lambertw
-from numpy.random import uniform,permutation
+from numpy.random import uniform
 
-import matplotlib.backends.backend_agg as agg
-import pygame
-import matplotlib.pyplot as plt
- 
 class sieve_experiment():
-
+    
     def __init__(self,m,n,iterations):
         self.n = n
+        self.m = m
         self.iterations=iterations
         phi0 = 1.*m/n
         smallPores = [m]
@@ -38,82 +35,3 @@ class sieve_experiment():
         c = log(phi0*exp(phi0))
         self.phi = lambertw(exp(-self.s+c)).real
         self.p = self.phi/(1.+self.phi)
-
-def CreatePlot(experiment,ind):
-    fig = plt.figure(figsize=[6, 4], dpi=100)
-    ax = fig.gca()
-    ax.plot(experiment.s,experiment.p,'c')
-    ax.plot(experiment.sims[:ind],experiment.simp[:ind],'xb')
-    plt.xlabel('time (t)',fontsize=12)
-    plt.title('Probability',fontsize=12)
-    ax.legend(('Theory','Experiment'),numpoints=1)
-    canvas = agg.FigureCanvasAgg(fig)
-    canvas.draw()
-    renderer = canvas.get_renderer()
-    raw_data = renderer.tostring_argb()
-    return(fig,raw_data,canvas)
-
-def ParticleFall(ind,surf):
-    size2 = canvas.get_width_height()
-
-    for j in range(0,size[1],2):
-        disp.blit(surf, ((size[0]-size2[0])/2,0))
-        x = int((pos[ind]+size[0]/2.)/2. + (size[0]/2.-pos[ind])*(cos(pi*j/size[1]))/2.)
-        pygame.draw.rect(disp,(200,0,200),(x,j,3,3))
-        pygame.display.flip()
-        pygame.draw.rect(disp,(0,0,0),(x,j,3,3))
-        
-def ParticleStuck(ind,surf):
-    for j in range(0,size[1]-21,2):
-        disp.blit(surf, ((size[0]-size2[0])/2,0))
-        x = int((pos[ind]+size[0]/2.)/2. + (size[0]/2.-pos[ind])*(cos(pi*j/(size[1]-21)))/2.)
-        pygame.draw.rect(disp,(200,0,200),(x,j,3,3))
-        pygame.display.flip()
-        pygame.draw.rect(disp,(0,0,0),(x,j,3,3))
-    pygame.draw.rect(disp,(255,0,0),(pos[ind],size[1]-23,2,2))
-    pygame.display.flip()
-
-# Draw the sieve.  Scramble the locations of the small pores.
-def DrawSieve(disp):
-    pos = map(int,size[0]/(m+n+1.)*r_[0:m+n]+2.5)
-    arrangement = permutation(n+m)
-
-    pygame.draw.rect(disp,(0,255,0),(0,size[1]-20,size[0],20))
-    for k in arange(len(pos)):
-        if (arrangement[k] >= m):
-            pygame.draw.rect(disp,(0,0,0),(pos[k]-1,size[1]-20,3,20))
-        else:
-            pygame.draw.rect(disp,(0,0,0),(pos[k],size[1]-20,1,20))
-    pygame.display.flip()
-    return(pos,arrangement)
-    
-size = (600,500)
-pygame.init()
-disp = pygame.display.set_mode(size)
-disp.fill((0,0,0))
-
-m = 100
-n = 20
-duration = 300
-
-experiment = sieve_experiment(m,n,duration)
-pos,arrangement = DrawSieve(disp)
-
-for its in range(1,duration):
-    fig,raw_data,canvas = CreatePlot(experiment,its)
-    if (its == 1):
-        size2 = canvas.get_width_height()
-        
-    surf = pygame.image.fromstring(raw_data, size2, "ARGB")
-    if (experiment.smallPores[its-1] == experiment.smallPores[its]):
-        # Fall into the next large pore.
-        q = int(uniform(0,m+n))
-        while (arrangement[q] < m):
-            q = int(uniform(0,m+n))
-        ParticleFall(q,surf)
-    else:
-        # Fall into the next small pore.
-        idx = argwhere(arrangement == experiment.smallPores[its])
-        idx = idx[0][0]
-        ParticleStuck(idx,surf)
-    plt.close(fig)
